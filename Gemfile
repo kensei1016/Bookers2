09bf63d10ff8e740eb9ad44720cb23a1c1b74337
野田健成 <kensei1016ab@gmail.com>
2020-01-08 15:06:47 +0900

    認証部分実装完了。（ログイン・ログアウト）

--- Gemfile
+++ Gemfile
@@ -60,3 +60,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# railsコンソールのテーブル表示を見やすくする
+gem 'hirb'
+gem 'hirb-unicode'
+
+gem 'devise'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+before_action :confiture_permitted_parameters, if: :devise_controller?
+
+protected
+
+def confiture_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+end
+
 end
