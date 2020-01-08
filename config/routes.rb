7cd4db959925fc09e4640b0f6188f2e06e2970b6
野田健成 <kensei1016ab@gmail.com>
2020-01-08 17:51:19 +0900

    [作業途中コミット] トップページ、aboutページを実装中。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#top'
+  get 'home/about'
   devise_for :users
   resources :books, only: [:new, :create, :index, :show]
   
