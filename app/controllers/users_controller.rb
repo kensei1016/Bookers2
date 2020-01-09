ad4b0302ed71f9263285115e50ff0c42520bf52b
野田健成 <kensei1016ab@gmail.com>
2020-01-09 13:05:55 +0900

    マイページ実装完了。Refileの環境構築。サンプルの投稿者画像を追加。

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def show
+    @book = Book.new
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+end
