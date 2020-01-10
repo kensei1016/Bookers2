4f00ab69c9c15f38eab84855410f79df45b7cbeb
kensei1016 <kensei1016ab@gmail.com>
2020-01-10 19:51:41 +0900

    top,aboutページに未ログインで遷移できるよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-before_action :authenticate_user!
-before_action :confiture_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top, :about]
+  before_action :confiture_permitted_parameters, if: :devise_controller?
 
-def after_sign_in_path_for(resource)
-  user_path(current_user.id)
-end
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
 
-protected
+  protected
 
-def confiture_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
-  devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :emailω])
-end
+  def confiture_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+  end
 
 end
