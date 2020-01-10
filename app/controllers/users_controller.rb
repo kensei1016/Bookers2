ad9675929491a0fb14267cd6c21fe9982eb941c7
kensei1016 <kensei1016ab@gmail.com>
2020-01-10 11:32:56 +0900

    ユーザ一覧からshowするとcurrent_userのマイページが開く件修正完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @book = Book.new
-    @books = Book.where(user_id: current_user.id)
-    @user = User.find(current_user.id)
+    @books = Book.where(user_id: params[:id])
+    @user = User.find(params[:id])
   end
 
   def edit
