b3b2864b534ed4edb445868ace02e09519724034
kensei1016 <kensei1016ab@gmail.com>
2020-01-22 00:43:45 +0900

    ・コメント機能のモデル、コントローラ作成 ・環境周りの準備 ・コメントの書き方変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.find_or_create_by!(email: "onigiri@example.com") do |user|
+  user.name = "onigiri"
+  user.introduction = 'サンプルサンプルサンプル'
+  user.password = 'password'
+end
+
+user_onigiri = User.find_by(email: "onigiri@example.com")
+
+5.times do |num|
+  book = Book.create!(
+    title: "Book#{num+1}",
+    body: "サンプルサンプルサンプル",
+    user_id: user_onigiri.id
+  )
+  
+  PostComment.create!(
+    comment: "コメントコメントコメント",
+    user_id: user_onigiri.id,
+    book_id: book.id
+  )
+end
