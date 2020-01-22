39252e81627487e54d602fd72bdd66185d71c202
野田健成 <kensei1016ab@gmail.com>
2020-01-22 17:53:30 +0900

    コメント空欄時にエラーを出すよう修正

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,11 +1,16 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    post_comment = current_user.post_comments.new(set_comment_params)
-    post_comment.book_id = book.id
-    if post_comment.save
-      redirect_to book_path(book.id), notice: "コメントを投稿しました"
+    @book = Book.find(params[:book_id])
+    @post_comment = current_user.post_comments.new(set_comment_params)
+    @post_comment.book_id = @book.id
+    if @post_comment.save
+      redirect_to book_path(@book.id), notice: "コメントを投稿しました"
     else
+      @book_new = Book.new
+      @user = User.find(current_user.id)
+      @post_comments = @book.post_comments
+
+      render "books/show"
     end
   end
 
