673053f58ced490d4f164ef05d24442893130370
kensei1016 <kensei1016ab@gmail.com>
2020-01-23 18:18:49 +0900

    seedの生成データを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,21 +21,62 @@ end
 user_onigiri = User.find_by(email: "onigiri@example.com")
 user_johnson = User.find_by(email: "johnson@example.com")
 
+bodys = [
+  'この本は僕が小さい頃母に読んでもらったものです。ひらがなしか出てこないので、老若男女問わず楽しメルと思います。',
+  '吾輩がこの本を読んだとき、あまりの衝撃に失神してしまった。一読の価値あり。',
+  'ワクワクした～ 読んでちょ',
+  'ほんの匂いが良かったと思いまする',
+  'なんていうかクリエイティブな気持ちにさせてくれたよ',
+  'I feel so happy!!',
+  'Do you like something cake?',
+  'I have been suffering in the Andes in the past.'
+]
+
+comments = [
+  'この本に感銘を受けました。',
+  '友人に勧めたいと思いました。',
+  '僕には理解できない領域だと感じました。',
+  'この本を読むと故郷の景色を思い出します（（泣））',
+  '僕は本を読むと眠くなるのですが、この本は5分間起きていられました！！',
+  'You have a dream.',
+  'I can jump from high places'
+]
+
 5.times do |num|
   book = Book.create!(
-    title: "Book#{num+1}",
-    body: "サンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプルサンプル",
+    title: "Book-#{user_onigiri.name}-#{num+1}",
+    body: bodys.sample,
     user_id: user_onigiri.id
   )
   
   PostComment.create!(
-    comment: "コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント",
+    comment: comments.sample,
     user_id: user_onigiri.id,
     book_id: book.id
   )
 
   PostComment.create!(
-    comment: "コメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメントコメント",
+    comment: comments.sample,
+    user_id: user_johnson.id,
+    book_id: book.id
+  )
+end
+
+3.times do |num|
+  book = Book.create!(
+    title: "Book-#{user_johnson.name}-#{num+1}",
+    body: bodys.sample,
+    user_id: user_johnson.id
+  )
+  
+  PostComment.create!(
+    comment: comments.sample,
+    user_id: user_onigiri.id,
+    book_id: book.id
+  )
+
+  PostComment.create!(
+    comment: comments.sample,
     user_id: user_johnson.id,
     book_id: book.id
   )
