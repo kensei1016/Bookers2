5bd58f47e4f1d02704a052609a1cff4405d0bd89
野田健成 <kensei1016ab@gmail.com>
2020-01-27 16:55:55 +0900

    レイアウト修正

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -13,21 +13,30 @@ $(document).on('turbolinks:load', function() {
       reader.onload = (function (theFile) {
         return function (e) {
           if (theFile.type.match('image.*')) {
-            var $html = ['<div class="d-inline-block mr-1 mt-1"><img class="img-thumbnail" src="', e.target.result, '" title="', escape(theFile.name), '" style="height:100px;" /><div class="small text-muted text-center">', escape(theFile.name), '</div></div>'].join('');// 画像では画像のプレビューとファイル名の表示
+            var $html = [
+              '<div class="d-inline-block mr-1 mt-1"><img class="img-thumbnail" src="',
+              e.target.result, '" title="',
+              escape(theFile.name),
+              '" style="height:100px;" /><div class="small text-muted text-center">',
+              escape(theFile.name),
+              '</div></div>'].join('');// 画像では画像のプレビューとファイル名の表示
           } else {
-            var $html = ['<div class="d-inline-block mr-1"><span class="small">', escape(theFile.name), '</span></div>'].join('');//画像以外はファイル名のみの表示
+            var $html = [
+              '<div class="d-inline-block mr-1"><span class="small">',
+              escape(theFile.name), 
+              '</span></div>'].join('');//画像以外はファイル名のみの表示
           }
           $('#preview').append($html);
         };
       })(f);
       reader.readAsDataURL(f);
     }
-    $(this).next('.custom-file-label').html(+ files.length + '個のファイルを選択しました');
+    $(this).next('.custom-file-label').html(+ files.length + ' file select');
   }
 
   //ファイルの取消
   $('.reset').click(function () {
-    $(this).parent().prev().children('.custom-file-label').html('ファイル選択...');
+    $(this).parent().prev().children('.custom-file-label').html('Book Image...');
     $('#preview').remove();
     $('.custom-file-input').val('');
   })
